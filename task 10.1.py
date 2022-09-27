d4b80c5cac7caa6cb260124f46c1515fd2de0203
KofE <90642629+kooofe@users.noreply.github.com>
2022-09-27 22:09:56 +0600

    Update task 10.1.py

--- task 10.1.py
+++ task 10.1.py
@@ -1,20 +1,22 @@
-def maxelement(arr):
-    no_of_rows = len(arr)
-    no_of_column = len(arr[0])
+def maxElement(arr):
+    numb_of_rows = len(arr)
+    numb_of_column = len(arr[0])
 
-    for i in range(no_of_rows):
+    for i in range(numb_of_rows):
 
         max1 = 0
-        for j in range(no_of_column):
+        for j in range(numb_of_column):
             if arr[i][j] > max1:
                 max1 = arr[i][j]
 
-        print(max1)
+        print("max element in row",max1)
 
 
-arr = [[3, 4, 1, 8],
-       [1, 4, 9, 11],
-       [76, 34, 21, 1],
-       [2, 1, 4, 5]]
+n = int(input("enter the n: "))
+m = int(input("enter the m: "))
+a = []
+print("enter the row by row")
+for i in range(n):
+    a.append(list(map(int, input().split())))
 
-maxelement(arr)
+maxElement(a)
