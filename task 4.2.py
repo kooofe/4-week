df6df420e587554bbdb1d4e2d6f39094f47f381b
KofE <90642629+kooofe@users.noreply.github.com>
2022-09-27 22:20:46 +0600

    Update task 4.2.py

--- task 4.2.py
+++ task 4.2.py
@@ -1,21 +1,16 @@
-n=int(input('введите кол строк:'))
-m=int(input('введите кол столбцов:'))
-a=[]
-for i in range(m):
-    b=[]
-    for j in range(n):
-        print('Введите [',i,',',j,'] элемент')
-        b.append(int(input()))
-    a.append(b)
+n = int(input("enter the n: "))
+m = int(input("enter the m: "))
+a = []
+print("enter the row by row")
+for i in range(n):
+    a.append(list(map(int, input().split())))
 
-print('исходный массив')
+print('origib array:')
 for i in range(m):
     for j in range(n):
         print(a[i][j], end=' ')
     print()
 
-a = [[1 if x>0 else 0 for x in i] for i in a]
+a = [[1 if x > 0 else 0 for x in i] for i in a]
 for i in range(len(a)):
-    print(*[a[i][x] if x<=i else '' for x in range(len(a[i]))],'')
-
-
+    print(*[a[i][x] if x <= i else '' for x in range(len(a[i]))], '')
